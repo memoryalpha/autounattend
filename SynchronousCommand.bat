782c6d4b345823cad6744c2a5d77452bf4ebcc21
Dennis Just <44903208+xWilli4m@users.noreply.github.com>
2023-01-02 15:58:47 +0100

    Create SynchronousCommand.bat

---
+++ SynchronousCommand.bat
@@ -0,0 +1,3 @@
+@echo off
+curl.exe --output "c:\install\firefox.exe" --url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/108.0.1/win64/en-US/Firefox%20Setup%20108.0.1.exe"
+c:\install\firefox.exe /S
