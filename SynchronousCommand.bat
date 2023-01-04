02ed24747f2f818da6ebf4039db14663d7cadb02
Dennis Just <44903208+xWilli4m@users.noreply.github.com>
2023-01-04 17:05:13 +0100

    Update SynchronousCommand.bat

--- SynchronousCommand.bat
+++ SynchronousCommand.bat
@@ -1,4 +1,12 @@
 @echo off
+mkdir c:\install
 cd c:\install
+
+
 curl --output "firefox.exe" "https://api.n3k1.de/exe/firefox.exe"
 firefox.exe /S
+
+
+curl --output "python.exe" "https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe"
+python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1 AppendPath=1
+cmd /C python -m pip install requests
